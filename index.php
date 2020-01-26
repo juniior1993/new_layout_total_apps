64580ebd1175869499be55b6e25da2ad3bd2a6e9
Darci Junior <juniior1993@gmail.com>
2020-01-26 01:41:35 -0300

    side bar e top nav completos

---
+++ index.php
@@ -0,0 +1,59 @@
+<!doctype html>
+<html lang="pt-BR">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dashboard</title>
+    <script src="https://kit.fontawesome.com/718e42f4f3.js" crossorigin="anonymous"></script>
+    <link href="https://fonts.googleapis.com/css?family=Montserrat:400,700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./assets/css/style.css">
+</head>
+<body>
+
+<div class="sidebar">
+    <div class="topo">
+        <h3>Total Apps</h3>
+    </div><!--topo-->
+    <div class="menu">
+        <ul>
+            <li><a href=""><i class="fas fa-angle-double-right"></i> Home</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><i class="fas fa-angle-double-right"></i> Financeiro</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><i class="fas fa-angle-double-right"></i> Producao</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><i class="fas fa-angle-double-right"></i> Afiliates</a></li>
+        </ul>
+        <ul>
+            <li><a href=""><i class="fas fa-angle-double-right"></i> Prefor</a></li>
+        </ul>
+    </div><!--    menu-->
+
+</div><!-- sidebar -->
+
+
+<!-- FIM DA SIDE BAR-->
+
+
+<div class="main-content">
+
+    <header>
+        <div class="pesquisa-campo">
+            <div class="icone-pesquisa"><i class="fas fa-search"></i></div><form><input type="text" /></form>
+        </div><!-- pesquisa-campo-->
+
+        <div class="clear"></div>
+    </header>
+
+</div><!--main-content-->
+
+<div class="clear"></div>
+
+
+</body>
+</html>
